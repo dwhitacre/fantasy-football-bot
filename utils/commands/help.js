2119e1415be6d63f61a140dd2248b5996eaa2772
Dan Whitacre <whitacre.74@osu.edu>
2021-10-16 17:14:04 -0400

    add help command

---
+++ utils/commands/help.js
@@ -0,0 +1,6 @@
+import Base from './_base'
+
+export default class Help extends Base {
+  id = 'help'
+  text = 'Type !list to see the available commands. Use the commands anywhere in your message, and I will respond!'
+}
--- utils/commands/index.js
+++ utils/commands/index.js
@@ -1,8 +1,10 @@
+import Help from './help'
 import List from './list'
 
-const commands = [List].map(Command => new Command()).filter(command => command.valid)
+const defaultCommands = [Help, List]
 
 export async function get(id) {
+  const commands = defaultCommands.map(Command => new Command()).filter(command => command.valid)
   if (id) return commands.find(command => command.id === id)
   return commands
 }

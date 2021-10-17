6191a36bb6e8167d7572e16c4b231a8666ccff66
Dan Whitacre <whitacre.74@osu.edu>
2021-10-16 21:18:02 -0400

    add cache to google sheets api calls

--- utils/middleware/sheets.js
+++ utils/middleware/sheets.js
@@ -3,16 +3,28 @@ import Base from '@/utils/commands/_base'
 
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_ID)
 
+const internals = {
+  cache: undefined,
+  time: undefined,
+  expiry: process.env.GOOGLE_SHEETS_CACHE_EXPIRY || 60000,
+}
+
 export default function sheets(req) {
   req.sheets = {
     doc,
     async rows() {
-      try {
-        await doc.useServiceAccountAuth(JSON.parse(process.env.GOOGLE_SHEETS_SA))
-        await doc.loadInfo()
-        return doc.sheetsByTitle['Commands']?.getRows()
-      } catch (err) {
-        req.log.error({ err }, 'error: sheets rows')
+      if (!internals.time || (internals.time && internals.expiry < Date.now() - internals.time)) {
+        internals.time = Date.now()
+        try {
+          await doc.useServiceAccountAuth(JSON.parse(process.env.GOOGLE_SHEETS_SA))
+          await doc.loadInfo()
+          return (internals.cache = doc.sheetsByTitle['Commands']?.getRows())
+        } catch (err) {
+          req.log.error({ err }, 'error: sheets rows')
+        }
+      } else if (internals.cache) {
+        req.log.debug({}, 'using sheets cache')
+        return internals.cache
       }
     },
   }
